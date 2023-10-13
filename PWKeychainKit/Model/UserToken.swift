e14b42836d1b4eb3c76470f561a24459fa05f914
jinwoong <baseball0020@gmail.com>
2023-10-13 23:13:31 +0900

    feat: add `UserToken` structure to handle a token item.

--- PWKeychainKit/Model/UserToken.swift
+++ PWKeychainKit/Model/UserToken.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+public struct UserToken {
+    var service: String
+    var token: String
+    
+    public init(service: String, token: String) {
+        self.service = service
+        self.token = token
+    }
+}
+
+extension UserToken: Queryable {
+    public var query: [String : Any] {
+        return [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecValueData as String: token,
+            kSecAttrService as String: service
+        ]
+    }
+}
